dd90904f8f0d2245bc04d693d4720af66c443e7f
Danie Martinoli <daniechris5@gmail.com>
2019-05-18 09:36:50 -0700

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,27 +1,26 @@
+
 dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"]
 
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index do |dwarve, index| 
+  dwarves.each_with_index do |dwarve, index|
     index += 1
-    puts "#{index} + #{dwarve}" 
+    puts "#{index} + #{dwarve}"
   end
 end
 
 def summon_captain_planet(planteer_calls)
-    planteer_calls.map! do |call| 
+    planteer_calls.map! do |call|
       call.capitalize + "!"
   end
 end
 
 def long_planeteer_calls(calls)
-  if #word is shorter than 4 characters
-    true 
-  else #word is longer that 4 characters  
-    false 
-
+  calls.any? do |calls|
+    calls.length > 4
+  end
 end
 
-#def find_the_cheese# code an argument here
-  # the array below is here to help
- # cheese_types = ["cheddar", "gouda", "camembert"]
-#end
+def find_the_cheese(contains_cheddar)
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  contains_cheddar.include?(cheese_types)
+end
